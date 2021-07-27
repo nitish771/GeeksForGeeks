b9d6b40fcaa0ef5edd15f6ea046a0b4a8f9d38b2
nitish771 <>
2021-07-27 12:41:58 +0530

    Add reverse array

---
+++ Basic/reverse-array.cpp
@@ -0,0 +1,33 @@
+/*
+	Problem : *
+	Logic : iterate throguh n//2 and reverse ith item to (n-i)th
+	Tags : Array
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void reverse_array(int arr[], int n){
+	for(int i=0; i<n/2; i++){
+		int t = arr[i];
+		arr[i] = arr[n-1-i];
+		arr[n-i-1] = t;
+	}
+}
+
+void print(int arr[], int n){
+	for (int i = 0; i < n; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+
+int main(){
+	int n = 9;
+	int arr[n] = {1,2,3,4,5,6,7,8,9};
+	reverse_array(arr, n);
+	print(arr, n);
+	return 0;
+}
